b9fce311dae818ddecdfcd919e4a476f9781dd9f
doniandroid <doniandroid@gmail.com>
2024-09-03 18:19:01 +0700

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,9 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+
+// Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAXpEVW8mxPHgAkDFv_BKkA1xX_C9V5IT4",
   authDomain: "absenku-fe810.firebaseapp.com",
@@ -13,5 +23,10 @@ const firebaseConfig = {
   appId: "1:135310368249:web:08ed2c9ea09c827bd6b128"
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inisialisasi Firestore
+const db = getFirestore(app);
+
+export { db };
