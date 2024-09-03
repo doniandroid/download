775de573aceee0653a348d07465c96a033c0e127
doniandroid <doniandroid@gmail.com>
2024-09-03 20:54:23 +0700

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,32 +1,24 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXpEVW8mxPHgAkDFv_BKkA1xX_C9V5IT4",
+    authDomain: "absenku-fe810.firebaseapp.com",
+    projectId: "absenku-fe810",
+    storageBucket: "absenku-fe810.appspot.com",
+    messagingSenderId: "135310368249",
+    appId: "1:135310368249:web:08ed2c9ea09c827bd6b128"
+  };
 
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore.js";
+  // Inisialisasi Firestore
+  const db = getFirestore(app);
 
-// Konfigurasi Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAXpEVW8mxPHgAkDFv_BKkA1xX_C9V5IT4",
-  authDomain: "absenku-fe810.firebaseapp.com",
-  projectId: "absenku-fe810",
-  storageBucket: "absenku-fe810.appspot.com",
-  messagingSenderId: "135310368249",
-  appId: "1:135310368249:web:08ed2c9ea09c827bd6b128"
-};
-
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-
-// Inisialisasi Firestore
-const db = getFirestore(app);
-
-export { db };
+  export { db };
